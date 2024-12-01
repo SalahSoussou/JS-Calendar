1085755c7f133460cddfb46111e9fb09328d4880
SalahSoussou <soussousalahiddine.com>
2024-12-01 19:18:22 +0100

    day 1

---
+++ main.js
@@ -0,0 +1,28 @@
+const cellSize = 200,
+  drawItemFunction = [];
+drawItemFunction[1] = drawStar;
+for (let day = 1; day <= 24; day++) {
+  const cnv = document.createElement("canvas");
+  cnv.width = cellSize;
+  cnv.height = cellSize;
+  document.body.append(cnv);
+  fillCell(day, cnv);
+}
+
+function fillCell(index, cnv) {
+  const ctx = cnv.getContext("2d"),
+    x = cnv.width / 2,
+    y = cnv.height / 2,
+    itemSize = cnv.width * 0.6,
+    drawItem = drawItemFunction[index];
+  drawItem
+    ? drawItem(ctx, x, y, itemSize)
+    : drawNumber(ctx, index, x, y, itemSize);
+}
+
+function drawNumber(ctx, value, x, y, size) {
+  ctx.font = size + "px Consolas";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText(value, x, y);
+}
